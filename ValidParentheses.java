878de4ff30e6f71cebc8b1359f20881ef84f955f
Ria <52408084+Ria36@users.noreply.github.com>
2023-04-10 10:21:21 +0530

    Create ValidParentheses.java

---
+++ ValidParentheses.java
@@ -0,0 +1,49 @@
+/* https://leetcode.com/problems/valid-parentheses/description/ */
+/*
+20. Valid Parentheses
+
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+An input string is valid if:
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+ 
+Example 1:
+
+Input: s = "()"
+Output: true
+
+Example 2:
+
+Input: s = "()[]{}"
+Output: true
+
+Example 3:
+
+Input: s = "(]"
+Output: false
+ 
+
+Constraints:
+
+1 <= s.length <= 104
+s consists of parentheses only '()[]{}'.
+*/
+
+class ValidParentheses {
+    public boolean isValid(String s) {
+    Deque<Character> stack = new ArrayDeque<>();
+
+    for (final char c : s.toCharArray())
+      if (c == '(')
+        stack.push(')');
+      else if (c == '{')
+        stack.push('}');
+      else if (c == '[')
+        stack.push(']');
+      else if (stack.isEmpty() || stack.pop() != c)
+        return false;
+
+    return stack.isEmpty();
+    }
+}
