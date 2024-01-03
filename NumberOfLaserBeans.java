9e6a48bde92444e84d90bd23ff82ece0938b1836
Ria <52408084+Ria36@users.noreply.github.com>
2024-01-03 14:37:04 +0530

    Create NumberOfLaserBeans.java

---
+++ NumberOfLaserBeans.java
@@ -0,0 +1,19 @@
+/* 2125. Number of Laser Beams in a Bank */
+
+class NumberOfLaserBeans
+  {
+    public int numberOfBeams(String[] bank) {
+    int ans = 0;
+    int prevOnes = 0;
+
+    for (final String row : bank) {
+      final int ones = (int) row.chars().filter(c -> c == '1').count();
+      if (ones > 0) {
+        ans += prevOnes * ones;
+        prevOnes = ones;
+      }
+    }
+
+    return ans;
+    }
+}
