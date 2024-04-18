9f8ff77500a8c475db160775e226da255bd7e99c
Ria <52408084+Ria36@users.noreply.github.com>
2024-04-18 20:57:31 +0530

    Create IslandPerimeter.java

---
+++ IslandPerimeter.java
@@ -0,0 +1,21 @@
+/* https://leetcode.com/problems/island-perimeter/?envType=daily-question&envId=2024-04-18 */
+/* 463. Island Perimeter */
+
+class IslandPerimeter {
+    public int islandPerimeter(int[][] grid) {
+    int islands = 0;
+    int neighbors = 0;
+
+    for (int i = 0; i < grid.length; ++i)
+      for (int j = 0; j < grid[0].length; ++j)
+        if (grid[i][j] == 1) {
+          ++islands;
+          if (i - 1 >= 0 && grid[i - 1][j] == 1)
+            ++neighbors;
+          if (j - 1 >= 0 && grid[i][j - 1] == 1)
+            ++neighbors;
+        }
+
+    return islands * 4 - neighbors * 2;
+    }
+}
