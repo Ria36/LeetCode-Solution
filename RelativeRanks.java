0653de513a28d6c07bd74f3aec89720ba1d32b6b
Ria <52408084+Ria36@users.noreply.github.com>
2024-05-08 11:14:03 +0530

    Create RelativeRanks.java

---
+++ RelativeRanks.java
@@ -0,0 +1,28 @@
+/* https://leetcode.com/problems/relative-ranks/submissions/1252371637/?envType=daily-question&envId=2024-05-08 */
+/* 506. Relative Ranks */
+
+class RelativeRanks {
+    public String[] findRelativeRanks(int[] nums) {
+    final int n = nums.length;
+    String[] ans = new String[n];
+    List<Integer> indices = new ArrayList<>();
+
+    for (int i = 0; i < n; ++i)
+      indices.add(i);
+
+    Collections.sort(indices, (a, b) -> nums[b] - nums[a]);
+
+    for (int i = 0; i < n; ++i)
+      if (i == 0)
+        ans[indices.get(0)] = "Gold Medal";
+      else if (i == 1)
+        ans[indices.get(1)] = "Silver Medal";
+      else if (i == 2)
+        ans[indices.get(2)] = "Bronze Medal";
+      else
+        ans[indices.get(i)] = String.valueOf(i + 1);
+
+    return ans;
+    }
+}
+
