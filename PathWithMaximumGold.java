2d5eb79a45557aa468721b650774ef020b6a37f2
Ria <52408084+Ria36@users.noreply.github.com>
2024-05-14 16:43:41 +0530

    Create PathWithMaximumGold.java

---
+++ PathWithMaximumGold.java
@@ -0,0 +1,30 @@
+/* https://leetcode.com/problems/path-with-maximum-gold/submissions/1257750229/?envType=daily-question&envId=2024-05-14 */
+
+/* 1219. Path with Maximum Gold */
+
+class PathWithMaximumGold {
+    public int getMaximumGold(int[][] grid) {
+    int ans = 0;
+
+    for (int i = 0; i < grid.length; ++i)
+      for (int j = 0; j < grid[0].length; ++j)
+        ans = Math.max(ans, dfs(grid, i, j));
+
+    return ans;
+  }
+
+  private int dfs(int[][] grid, int i, int j) {
+    if (i < 0 || j < 0 || i == grid.length || j == grid[0].length)
+      return 0;
+    if (grid[i][j] == 0)
+      return 0;
+
+    final int gold = grid[i][j];
+    grid[i][j] = 0; // Mark as visited.
+    final int maxPath = Math.max(Math.max(dfs(grid, i + 1, j), dfs(grid, i - 1, j)),
+                                 Math.max(dfs(grid, i, j + 1), dfs(grid, i, j - 1)));
+    grid[i][j] = gold;
+    return gold + maxPath;
+    }
+}
+
