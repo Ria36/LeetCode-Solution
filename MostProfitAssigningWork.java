b7ee0870ffa5b6d50152993558ee8cd1e201b400
Ria <52408084+Ria36@users.noreply.github.com>
2024-06-18 22:47:09 +0530

    Create MostProfitAssigningWork.java

---
+++ MostProfitAssigningWork.java
@@ -0,0 +1,26 @@
+/* https://leetcode.com/problems/most-profit-assigning-work/?envType=daily-question&envId=2024-06-18 */
+/* 826. Most Profit Assigning Work */
+
+class MostProfitAssigningWork {
+    public int maxProfitAssignment(int[] difficulty, int[] profit, int[] worker) {
+    int ans = 0;
+    List<Pair<Integer, Integer>> jobs = new ArrayList<>();
+
+    for (int i = 0; i < difficulty.length; ++i)
+      jobs.add(new Pair<>(difficulty[i], profit[i]));
+
+    Collections.sort(jobs, Comparator.comparing(Pair::getKey));
+    Arrays.sort(worker);
+
+    int i = 0;
+    int maxProfit = 0;
+
+    for (final int w : worker) {
+      for (; i < jobs.size() && w >= jobs.get(i).getKey(); ++i)
+        maxProfit = Math.max(maxProfit, jobs.get(i).getValue());
+      ans += maxProfit;
+    }
+
+    return ans;
+    }
+}
