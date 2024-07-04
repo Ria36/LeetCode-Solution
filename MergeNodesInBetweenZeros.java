3a6682cbf2f52f377ace4d4d8533566357b0ba32
Ria <52408084+Ria36@users.noreply.github.com>
2024-07-04 12:09:52 +0530

    Create MergeNodesInBetweenZeros.java

---
+++ MergeNodesInBetweenZeros.java
@@ -0,0 +1,28 @@
+/* https://leetcode.com/problems/merge-nodes-in-between-zeros/?envType=daily-question&envId=2024-07-04 */
+/* 2181. Merge Nodes in Between Zeros */
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class MergeNodesInBetweenZeros {
+    public ListNode mergeNodes(ListNode head) {
+    if (head == null)
+      return null;
+    if (head.next.val == 0) {
+      ListNode node = new ListNode(head.val);
+      node.next = mergeNodes(head.next.next);
+      return node;
+    }
+
+    ListNode next = mergeNodes(head.next);
+    next.val += head.val;
+    return next;
+    }
+}
