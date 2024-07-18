62b6a293170086e43b09293a2fae3451e463dbb3
Ria <52408084+Ria36@users.noreply.github.com>
2024-07-18 23:12:26 +0530

    Create NumberOfGoodLeafNodesPairs.java

---
+++ NumberOfGoodLeafNodesPairs.java
@@ -0,0 +1,50 @@
+/* https://leetcode.com/problems/number-of-good-leaf-nodes-pairs/submissions/1325472192/?envType=daily-question&envId=2024-07-18 */
+/* 1530. Number of Good Leaf Nodes Pairs */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class NumberOfGoodLeafNodesPairs {
+  public int countPairs(TreeNode root, int distance) {
+    dfs(root, distance);
+
+    return ans;
+  }
+
+  private int ans = 0;
+
+  private int[] dfs(TreeNode root, int distance) {
+    int[] d = new int[distance + 1]; // {distance: the number of leaf nodes}
+    if (root == null)
+      return d;
+    if (root.left == null && root.right == null) {
+      d[0] = 1;
+      return d;
+    }
+
+    int[] dl = dfs(root.left, distance);
+    int[] dr = dfs(root.right, distance);
+
+    for (int i = 0; i < distance; ++i)
+      for (int j = 0; j < distance; ++j)
+        if (i + j + 2 <= distance)
+          ans += dl[i] * dr[j];
+
+    for (int i = 0; i < distance; ++i)
+      d[i + 1] = dl[i] + dr[i];
+
+    return d;
+  }
+}
