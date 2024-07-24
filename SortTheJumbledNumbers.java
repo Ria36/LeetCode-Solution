5f5ea000a9e34f15cb3de123bd8130219d55efed
Ria <52408084+Ria36@users.noreply.github.com>
2024-07-24 16:06:14 +0530

    Create SortTheJumbledNumbers.java

---
+++ SortTheJumbledNumbers.java
@@ -0,0 +1,27 @@
+/* https://leetcode.com/problems/sort-the-jumbled-numbers/?envType=daily-question&envId=2024-07-24 */
+/* 2191. Sort the Jumbled Numbers */
+
+class SortTheJumbledNumbers {
+  public int[] sortJumbled(int[] mapping, int[] nums) {
+    List<Integer> ans = new ArrayList<>();
+    TreeMap<Integer, List<Integer>> mappedToOriginalNums = new TreeMap<>();
+
+    for (final int num : nums) {
+      final int mapped = getMapped(num, mapping);
+      mappedToOriginalNums.putIfAbsent(mapped, new ArrayList<>());
+      mappedToOriginalNums.get(mapped).add(num);
+    }
+
+    for (List<Integer> originalNums : mappedToOriginalNums.values())
+      ans.addAll(originalNums);
+
+    return ans.stream().mapToInt(Integer::intValue).toArray();
+  }
+
+  private int getMapped(int num, int[] mapping) {
+    StringBuilder sb = new StringBuilder();
+    for (final char c : String.valueOf(num).toCharArray())
+      sb.append(mapping[c - '0']);
+    return Integer.parseInt(sb.toString());
+  }
+}
