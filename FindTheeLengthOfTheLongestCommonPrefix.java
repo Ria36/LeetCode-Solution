3598b61064c3ae9f9a3cfe1c0b65a05bab3fa02c
Ria <52408084+Ria36@users.noreply.github.com>
2024-09-24 19:34:08 +0530

    Create FindTheeLengthOfTheLongestCommonPrefix.java

---
+++ FindTheeLengthOfTheLongestCommonPrefix.java
@@ -0,0 +1,48 @@
+/* https://leetcode.com/problems/find-the-length-of-the-longest-common-prefix/?envType=daily-question&envId=2024-09-24 */
+/* 3043. Find the Length of the Longest Common Prefix */
+
+class TrieNode {
+  public TrieNode[] children = new TrieNode[10];
+}
+
+class Trie {
+  public void insert(final String word) {
+    TrieNode node = root;
+    for (final char c : word.toCharArray()) {
+      final int i = c - '0';
+      if (node.children[i] == null)
+        node.children[i] = new TrieNode();
+      node = node.children[i];
+    }
+  }
+
+  public int search(final String word) {
+    int prefixLength = 0;
+    TrieNode node = root;
+    for (final char c : word.toCharArray()) {
+      final int i = c - '0';
+      if (node.children[i] == null)
+        break;
+      node = node.children[i];
+      ++prefixLength;
+    }
+    return prefixLength;
+  }
+
+  private TrieNode root = new TrieNode();
+}
+
+class FindTheeLengthOfTheLongestCommonPrefix {
+  public int longestCommonPrefix(int[] arr1, int[] arr2) {
+    int ans = 0;
+    Trie trie = new Trie();
+
+    for (final int num : arr1)
+      trie.insert(Integer.toString(num));
+
+    for (final int num : arr2)
+      ans = Math.max(ans, trie.search(Integer.toString(num)));
+
+    return ans;
+  }
+}
